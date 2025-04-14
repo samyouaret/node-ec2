e514acf18145e4cbaafa468ef566057efd4d45d3
Samy Ouaret <samyouaret13@gmail.com>
2025-04-14 22:23:23 +0100

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,6 @@ server.get('/', (req, res) => {
     });
 });
 
-const PORT = 80;
+const PORT = process.env.PORT || 8080;
 const HOST = 'localhost';
 server.listen(PORT, () => console.log(`starting server at http://${HOST}:${PORT}`))
